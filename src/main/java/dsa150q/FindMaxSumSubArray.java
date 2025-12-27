9457356d15d95344e90f5903d5c2277c4d9d0b11
mohan vishnu <mohanvishnu565@outlook.com>
2025-12-27 16:43:29 +0530

    practices Kadane’s Algorithm optimise

--- src/main/java/dsa150q/FindMaxSumSubArray.java
+++ src/main/java/dsa150q/FindMaxSumSubArray.java
@@ -6,6 +6,8 @@ public class FindMaxSumSubArray {
     public static void main(String[] args) {
         int[] array = {1,3,-12,8,5,4,9,-11,6};
         System.out.println(Arrays.toString(findMaxSubArray(array)));
+        practices(new int[]{1, -2, 4, 3, 7, 1});
+        practices(array);
     }
     private static int[] findMaxSubArray(int[] array) {
         int maxSum=array[0], currentSum=array[0];
@@ -23,5 +25,22 @@ public class FindMaxSumSubArray {
             }
         }
         return Arrays.copyOfRange(array,maxSumStarts,maxSumEnds+1);
+    }
+    private static void practices(int[] array){
+        int maxSum=array[0],currSum=array[0];
+        int maxStart=0,maxEnd=0,tempStart=0;
+        for(int i=1;i<array.length;i++){
+            if(array[i]>currSum+array[i]){
+                currSum = array[i];
+                tempStart=i;
+            }else currSum+=array[i];
+
+            if(currSum>maxSum){
+                maxSum = currSum;
+                maxStart = tempStart;
+                maxEnd = i;
+            }
+        }
+        System.out.println(Arrays.toString(Arrays.copyOfRange(array,maxStart,maxEnd+1)));
     }
 }
